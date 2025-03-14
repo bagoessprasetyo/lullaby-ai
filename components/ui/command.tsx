8cc525ebdc4a9541710dfa69144a746d1e7ed5d3
bagoessprasetyo <prasetyobagus7@gmail.com>
2025-03-15 02:09:36 +0700

    fix command

--- components/ui/command.tsx
+++ components/ui/command.tsx
@@ -9,19 +9,22 @@ import { cn } from "@/lib/utils"
 import { Dialog, DialogContent } from "@/components/ui/dialog"
 
 // The issue might be with the forwardRef type definition
-const Command = React.forwardRef<
-  React.ElementRef<typeof CommandPrimitive>,
-  React.ComponentPropsWithoutRef<typeof CommandPrimitive>
->(({ className, ...props }, ref) => (
-  <CommandPrimitive
-    ref={ref}
-    className={cn(
-      "flex h-full w-full flex-col overflow-hidden rounded-md bg-popover text-popover-foreground",
-      className
-    )}
-    {...props}
-  />
-))
+// Alternative fix for the Command component
+type CommandPrimitiveElement = React.ElementRef<typeof CommandPrimitive>
+type CommandPrimitiveProps = React.ComponentPropsWithoutRef<typeof CommandPrimitive>
+
+const Command = React.forwardRef<CommandPrimitiveElement, CommandPrimitiveProps>(
+  ({ className, ...props }, ref) => (
+    <CommandPrimitive
+      ref={ref}
+      className={cn(
+        "flex h-full w-full flex-col overflow-hidden rounded-md bg-popover text-popover-foreground",
+        className
+      )}
+      {...props}
+    />
+  )
+)
 Command.displayName = CommandPrimitive.displayName
 
 const CommandDialog = ({ children, ...props }: DialogProps) => {
