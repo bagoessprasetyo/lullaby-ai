ad4de38a0994b26b1167a47fd25b21df9bcbc01c
bagoessprasetyo <prasetyobagus7@gmail.com>
2025-03-18 22:31:47 +0700

    upd

--- lib/rate-limiter.ts
+++ lib/rate-limiter.ts
@@ -6,3 +6,10 @@ export const rateLimiter = new Ratelimit({
   limiter: Ratelimit.slidingWindow(5, '60 s'),
   analytics: true
 });
+
+const redis = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL?.startsWith('https://') 
+    ? process.env.UPSTASH_REDIS_REST_URL 
+    : `https://${process.env.UPSTASH_REDIS_REST_URL}`,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN,
+});
