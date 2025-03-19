c328d3770bb6a21c815cad71684990f79b7e3173
bagoessprasetyo <prasetyobagus7@gmail.com>
2025-03-19 18:31:57 +0700

    fix sonner

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,10 +5,17 @@ import { Inter } from 'next/font/google'
 import { AuthProvider } from "@/components/AuthProvider";
 import { QueryProvider } from "@/lib/providers/query-provider";
 import { Analytics } from '@vercel/analytics/next';
-import { Toaster } from 'sonner';
 import { SpeedInsights } from "@vercel/speed-insights/next"
-// import { Analytics } from '@upstash/ratelimit';
-// import { Toaster } from "@/components/ui/toaster";
+import { Suspense } from 'react';
+import dynamic from 'next/dynamic';
+
+const Toaster = dynamic(
+  () => import('sonner').then((mod) => mod.Toaster),
+  { 
+    ssr: false,
+    loading: () => null
+  }
+);
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -24,17 +31,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body
-        className={`${inter} dark bg-black antialiased`}
-      >
-        <AuthProvider>
-          <QueryProvider>
-            {children}
-            <Toaster />
-            <Analytics />
-            <SpeedInsights/>
-          </QueryProvider>
-        </AuthProvider>
+      <body className={`${inter} dark bg-black antialiased`}>
+        <Suspense fallback={<div className="flex-1 flex items-center justify-center">Loading...</div>}>
+          <AuthProvider>
+            <QueryProvider>
+              {children}
+              <Analytics />
+              <SpeedInsights/>
+            </QueryProvider>
+          </AuthProvider>
+        </Suspense>
+        <Toaster />
       </body>
     </html>
   )
